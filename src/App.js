ddcceca23bdfba19d91d64b2b4f1dc9ac31d965e
alishalawani <alishalawani@gmail.com>
2020-09-30 12:51:13 -0500

    displayed questions on the page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 import './App.css';
-
+import Quiz from './components/Quiz'
 function App() {
   return (
     <div className="App">
       <h1>This is the Main Quiz Panel</h1>
+    <Route path='/quiz' component={Quiz}/>
     </div>
   );
 }
