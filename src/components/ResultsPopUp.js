c1c84893434bb74aea18118abf4ffdd2cd0631f6
Will Tidwell <willtomtid@gmail.com>
2020-09-30 15:53:30 -0400

    added fontawesome icons

--- src/components/ResultsPopUp.js
+++ src/components/ResultsPopUp.js
@@ -1,45 +1,106 @@
 import React from "react";
 import ProgressBar from "react-animated-progress-bar";
 import "./resultsPopUp.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
+import { faInstagramSquare } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 const ResultsPopUp = () => {
   return (
     <div className="resultsPopUp">
-      <h1>Your Results</h1>
-      <div className="allResultsBars">
-        <div className="resultsBar">
-          <p>Federal</p>
-          <ProgressBar width="100" trackWidth="13" percentage="100" />
+      <FontAwesomeIcon icon={faTimes} className="exitLogo" />
+      <div className="resultsPopUpMain">
+        <h1>Your Quiz Results</h1>
+        <div className="allResultsBars">
+          <div className="resultsBar">
+            <p>Federal</p>
+            <ProgressBar
+              width="100"
+              trackWidth="13"
+              percentage="100"
+              defColor={{
+                good: "red",
+                excellent: "blue",
+              }}
+            />
+          </div>
+          <div className="resultsBar">
+            <p>State</p>
+            <ProgressBar
+              width="100"
+              trackWidth="13"
+              percentage="100"
+              defColor={{
+                good: "red",
+                excellent: "blue",
+              }}
+            />
+          </div>
+          <div className="resultsBar">
+            <p>Local</p>
+            <ProgressBar
+              width="100"
+              trackWidth="13"
+              percentage="0"
+              defColor={{
+                good: "red",
+                excellent: "blue",
+              }}
+            />
+          </div>
+          <div className="resultsBar">
+            <p>Judicial</p>
+            <ProgressBar
+              width="100"
+              trackWidth="13"
+              percentage="50"
+              defColor={{
+                good: "red",
+                excellent: "blue",
+              }}
+            />
+          </div>
+          <div className="resultsBar">
+            <p>Measures</p>
+            <ProgressBar
+              width="100"
+              trackWidth="13"
+              percentage="0"
+              defColor={{
+                good: "red",
+                excellent: "blue",
+              }}
+            />
+          </div>
+          <div className="resultsBar">
+            <p>General</p>
+            <ProgressBar
+              width="100"
+              trackWidth="13"
+              percentage="100"
+              defColor={{
+                good: "red",
+                excellent: "blue",
+              }}
+            />
+          </div>
         </div>
-        <div className="resultsBar">
-          <p>State</p>
-          <ProgressBar width="100" trackWidth="13" percentage="100" />
+        <div className="resultsInfo">
+          <h1>Your Quiz Results</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Porttitor
+            urna, ut mi sed scelerisque ullamcorper nisi, vulputate orci. Nunc
+            habitant vitae rutrum pharetra ut leo. Interdum ornare donec a
+            molestie morbi sed non, et ac.
+          </p>
+          <br />
+          <p>Shared with friends</p>
+          <FontAwesomeIcon icon={faFacebookSquare} className="socialLogo"/>
+          <FontAwesomeIcon icon={faInstagramSquare} className="socialLogo"/>
+          <FontAwesomeIcon icon={faTwitter} className="socialLogo"/>
         </div>
-        <div className="resultsBar">
-          <p>Local</p>
-          <ProgressBar width="100" trackWidth="13" percentage="0" />
-        </div>
-        <div className="resultsBar">
-          <p>Judicial</p>
-          <ProgressBar width="100" trackWidth="13" percentage="50" />
-        </div>
-        <div className="resultsBar">
-          <p>Measures</p>
-          <ProgressBar width="100" trackWidth="13" percentage="0" />
-        </div>
-        <div className="resultsBar">
-          <p>General</p>
-          <ProgressBar width="100" trackWidth="13" percentage="100" />
-        </div>
-      </div>
-      <div className="resultsInfo">
-        <h1>Your Results</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Porttitor
-          urna, ut mi sed scelerisque ullamcorper nisi, vulputate orci. Nunc
-          habitant vitae rutrum pharetra ut leo. Interdum ornare donec a
-          molestie morbi sed non, et ac.
-        </p>
       </div>
     </div>
   );
