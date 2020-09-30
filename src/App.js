6ccae62a613a9873d88a6dbbd204cdef57978b30
Will Tidwell <willtomtid@gmail.com>
2020-09-30 19:30:30 -0400

    bones of currentofficials

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Quiz from "./components/Quiz";
 import Results from "./components/routes/Results";
 import UserForm from "./components/routes/UserForm";
+import CurrentOfficials from "./components/routes/CurrentOfficials";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" component={UserForm} />
         <Route path="/quiz" component={Quiz} />
         <Route path="/results" component={Results} />
+        <Route path="/currentofficials" component={CurrentOfficials} />
       </Switch>
     </div>
   );
---
+++ src/components/routes/CurrentOfficials.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './currentOfficials.css'
+
+const CurrentOfficials = () => {
+  return (
+    <div className="currentOfficials">
+      <h1>Current Officials</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Porttitor urna,
+        ut mi sed scelerisque ullamcorper nisi, vulputate orci. Nunc habitant
+        vitae rutrum pharetra ut leo. Interdum ornare donec a molestie morbi sed
+        non, et ac.
+      </p>
+    </div>
+  );
+};
+
+export default CurrentOfficials;
